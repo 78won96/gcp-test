42dd3bd40f3c88dc1523467ebf4fdebf0968965f
juwon.lee <juwon.lee@-infra.com>
2024-10-17 09:59:57 +0900

    new

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log(`${package.name} received a request.`);
-  res.send(`Congratulations, you successfully deployed a container image to Cloud Run!`);
+  res.send(`Congratulations, you successfully deployed a container image to Cloud Run!-new`);
 });
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`${package.name} listening on port: ${port}`);
