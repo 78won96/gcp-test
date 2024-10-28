21d417e3397f59d3e458a7dcf83c5a76333e73fc
juwon.lee <juwon.lee@-infra.com>
2024-10-28 09:39:16 +0900

    add prisma

--- twistcli_old/twistcli.sh
+++ twistcli_old/twistcli.sh
@@ -9,3 +9,10 @@ curl -k -u $$prismauser:$$prismapassword= --output /twistcli $address
 chmod a+x /twistcli
 /twistcli images scan --details -address $address -u $$prismauser -p $$prismapassword
 
+
+
+
+
+curl -k -u ""294ef65d-0aa8-46d2-ac16-9f9571fd0be1:jb7I9+dP2zBO1+5MTeEqARKP7jE=" --output /twistcli "https://asia-northeast1.cloud.twistlock.com/japan-1167212692/api/v1/util/twistcli""
+
+curl -k -u 294ef65d-0aa8-46d2-ac16-9f9571fd0be1:jb7I9+dP2zBO1+5MTeEqARKP7jE= --output /twistcli https://asia-northeast1.cloud.twistlock.com/japan-1167212692
